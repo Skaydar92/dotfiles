f88d65a2f4e708ce2f1f3c6869f0308422cf00da
Skaydar92 <ftwbusch04@gmail.com>
2026-01-06 18:21:07 +0100

    added sorting script

---
+++ scritps/sort.sh
@@ -0,0 +1,11 @@
+cd /home/felix/Downloads
+
+mkdir -p /home/felix/Downloads/Compressed
+mkdir -p /home/felix/Downloads/Images
+mkdir -p /home/felix/Downloads/Pictures
+mkdir -p /home/felix/Downloads/Documents
+
+mv *.zip /home/felix/Downloads/Compressed/
+mv *.iso /home/felix/Downloads/Images/
+mv *.jpg /home/felix/Downloads/Pictures/
+mv *.pdf /home/felix/Downloads/Documents/
